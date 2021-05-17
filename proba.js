9b09fba6595efe49c324eb6dcec02029b14d5d69
MUHAMMADYUSUF0217 <74308787+MUHAMMADYUSUF0217@users.noreply.github.com>
2021-05-17 22:14:12 +0500

    Delete proba.js
    I deleted  js file

--- proba.js
+++
@@ -1,9 +0,0 @@
-// let object = {
-// 	name:"Muhammadyusuf"
-// 	centre: "Najot Ta'lim"
-// }
-
-// let {name: ism} = object
-// console.log(ism)
-
-throw new ReferenceError("siz login termadingiz")
